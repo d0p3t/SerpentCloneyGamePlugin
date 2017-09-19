bfd56271ae7e066b4bc3dba1ecde549a75445612
Remco Troost <remcotroost@outlook.com>
2017-09-19 05:21:47 +0200

    Initial commit

---
+++ files/serpent_Cloney_game.py
@@ -0,0 +1,49 @@
+from serpent.game import Game
+
+from .api.api import CloneyAPI
+
+from serpent.utilities import Singleton
+
+
+class SerpentCloneyGame(Game, metaclass=Singleton):
+
+    def __init__(self, **kwargs):
+        kwargs["platform"] = "steam"
+
+        kwargs["window_name"] = "Cloney"
+
+        kwargs["app_id"] = "400030"
+        kwargs["app_args"] = None
+
+
+        super().__init__(**kwargs)
+
+        self.api_class = CloneyAPI
+        self.api_instance = None
+
+    @property
+    def screen_regions(self):
+        regions = {
+            "SAMPLE_REGION": (0, 0, 0, 0)
+        }
+
+        return regions
+
+    @property
+    def ocr_presets(self):
+        presets = {
+            "SAMPLE_PRESET": {
+                "extract": {
+                    "gradient_size": 1,
+                    "closing_size": 1
+                },
+                "perform": {
+                    "scale": 10,
+                    "order": 1,
+                    "horizontal_closing": 1,
+                    "vertical_closing": 1
+                }
+            }
+        }
+
+        return presets
